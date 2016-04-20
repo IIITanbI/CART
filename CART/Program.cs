e9474b6f4ea2757bd5dd4616cd91b796adf97d3e
IIITanbI <artyom.kuis@gmail.com>
2016-04-20 17:09:23 +0300

    init

---
+++ CART/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DT_Algorithm;
+using System.IO;
+
+namespace CART
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Tree tree = new Tree();
+            DataSet dataSet = new DataSet();
+
+
+            var lines = File.ReadAllLines("input.txt");
+
+            var ht = lines[0].Split(new char[] { '\t' }, StringSplitOptions.RemoveEmptyEntries);
+            var hn = lines[1].Split(new char[] { '\t' }, StringSplitOptions.RemoveEmptyEntries);
+
+
+            if (hn.Length != ht.Length)
+            {
+                throw new ArgumentException("azaza");
+            }
+
+            foreach (var line in lines.Skip(2))
+            {
+                var item = new Item();
+
+                var values = line.Split(new char[] { '\t' }, StringSplitOptions.RemoveEmptyEntries);
+                if (values.Length != ht.Length)
+                {
+                    throw new ArgumentException("azaza2");
+                }
+
+                for (int i = 0; i < values.Length; i++)
+                {
+                    string name = hn[i];
+                    string value = values[i];
+                    AttributeType type;
+                    if (ht[i] == "c") type = AttributeType.CategoricalNominal;
+                    else type = AttributeType.Numerical;
+
+                    item.Attributes[name] = new MyAttribute(name, value, type);
+                }
+
+                dataSet.Items.Add(item);
+            }
+
+            tree.Data = dataSet;
+            tree.ClassificationAttributeName = hn.Last();
+            tree.Build();
+            //var gini = Utility.Gini(dataSet, hn.Last());
+
+            Console.WriteLine();
+        }
+    }
+}
---
+++ ConsoleApplication1/Source.cpp
@@ -0,0 +1,10 @@
+#include <cstdio>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	cout << _I32_MAX << endl;
+	cout << _I64_MAX << endl;
+	return 0;
+}
